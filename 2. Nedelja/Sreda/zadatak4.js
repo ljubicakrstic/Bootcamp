3df40fe84117077ae4bac5f7f42020b208776638
ljubicakrstic <ljubica.z.krstic@gmail.com>
2020-07-20 18:40:43 +0200

    gotov zadatak4

---
+++ 2. Nedelja/Sreda/zadatak4.js
@@ -0,0 +1,16 @@
+//DELJIVO SA 5 DO 1.000
+
+
+{
+    let a = 1; 
+    let b = 0;
+    while(a <= 1000){
+        if(b % 5 == 0){
+            console.log(b);
+            a++;
+        }
+       b++;
+    }
+}
+
+
