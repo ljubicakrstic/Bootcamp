f39cf0149cba78a4d4c4affa03ca121d6c9e796a
ljubicakrstic <ljubica.z.krstic@gmail.com>
2020-07-20 17:56:59 +0200

    gotov zadatak3

---
+++ 2. Nedelja/Sreda/zadatak3.js
@@ -0,0 +1,11 @@
+//DELJIVO SA 3
+
+{
+    let a = 5;
+
+    if(a % 3 != 0){
+        console.log("Broj "+ a + " nije deljiv sa 3");
+    }else{
+        console.log("Broj "+ a + " je deljiv sa 3");
+    }
+}
