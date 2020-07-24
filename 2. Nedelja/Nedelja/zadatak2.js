10ab75abb1f415e4a9773d299bebd3d6500f5547
ljubicakrstic <ljubica.z.krstic@gmail.com>
2020-07-24 18:59:18 +0200

    FizzBuzz valjda konacno resenje

--- 2. Nedelja/Nedelja/zadatak2.js
+++ 2. Nedelja/Nedelja/zadatak2.js
@@ -1,4 +1,28 @@
+//red po red;
 
+{
+    let a = 1;
+    while(a <= 100){
+        let odgovor = "";
+        if(a % 3 == 0){
+            odgovor += "Fizz";
+        }
+        if(a % 5 == 0){
+            odgovor += "Buzz";
+        }
+         if(a % 2 == 0){
+             odgovor += "Puzz";
+         }
+        if(odgovor == ""){
+            odgovor += a;
+        }
+        console.log(odgovor);
+        a++;
+    }
+    
+}
+
+//da bude u liniji sa zarezima;
 
 {
     let a = 1;
@@ -10,8 +34,15 @@
         if(a % 5 == 0){
             odgovor += "Buzz";
         }
-        odgovor += a;
-        a++;
+        if(a % 3 != 0 && a % 5 != 0){
+            odgovor += a;
+        }
+        if(a !== 100){
+        odgovor += ", ";
+        }
+        a++; 
     }
     console.log(odgovor);
 }
+
+
